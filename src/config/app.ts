eb3b756fd584cf71ff0db368d06e160ea9440d9f
alanbruno1994 <bruno47rios@outlook.com>
2022-01-12 15:04:51 -0300

    feat: insert setups in app

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,4 +1,9 @@
 import express from "express";
+import bodyParse from "../middleware/bodyParse";
+import setupRouter from "./configRouter";
+import setupGraphQl from "./setupGraphQl";
 const app = express();
-
+bodyParse(app);
+setupGraphQl(app);
+setupRouter(app);
 export default app;
